0b240d0da1a02589c5c8eda16c8c56009dcbec3d
Ewilan Rivière <ewilan.riviere@gmail.com>
2024-01-11 09:34:07 +0100

    init

---
+++ app/Models/SushiTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Sushi\Sushi;
+
+class SushiTest extends Model
+{
+    use HasFactory;
+    use Sushi;
+
+    protected $rows = [
+        [
+            'abbr' => 'NY',
+            'name' => 'New York',
+        ],
+        [
+            'abbr' => 'CA',
+            'name' => 'California',
+        ],
+    ];
+}
